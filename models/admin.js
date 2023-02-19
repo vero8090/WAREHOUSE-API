144d5b7e549317ae45396ab3f4ac78283a089fa7
vero8090 <rochan.sm@gmail.com>
2023-02-19 16:37:30 +0700

    testing-comparation

--- models/admin.js
+++ models/admin.js
@@ -21,6 +21,7 @@ module.exports = (sequelize, DataTypes) => {
     password: DataTypes.STRING,
     nama: DataTypes.STRING,
     role: DataTypes.STRING,
+    test:DataTypes.INTEGER,
     phone_number: DataTypes.INTEGER,
     photo_profile: DataTypes.STRING,
     createdAt:{
